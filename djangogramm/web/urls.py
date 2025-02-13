5a4ec3d33401e74278856e4b017aece800d1c516
olegsedak-education <olegsedak-education@users.noreply.github.com>
2025-02-13 21:31:58 +0200

    djangogramm app sync with dev in ide

--- djangogramm/web/urls.py
+++ djangogramm/web/urls.py
@@ -33,12 +33,7 @@ urlpatterns = [
          include([
              path("", views.users_list, name="users_list"),
              path("<int:user_id>/",
-                  include([
-                      path("",views.user_profile, name="user_profile"),
-                      path("follow/", views.follow_user, name="follow_user"),
-                      path("unfollow/", views.unfollow_user, name="unfollow_user"),
-                  ])
-            ),
+                  include(defined_user_urls)),
              ])
          ),
     path("posts/",
@@ -46,16 +41,10 @@ urlpatterns = [
              path("", views.posts_list, name="posts_list"),
              path("create/", views.create_post, name="create_post"),
              path("<int:post_id>/",
-                  include([
-                      path("", views.post_detail, name="post_detail"),
-                      path("like/", views.like_post, name="like_post"),
-                      path("unlike/", views.unlike_post, name="unlike_post"),
-                      path("dislike/", views.dislike_post, name="dislike_post"),
-                      path("undislike/", views.undislike_post, name="undislike_post")
-                  ]),
-              ),
+                  include(defined_post_urls),
+                  ),
          ]),
     ),
     path("feed/", views.feed, name="feed"),
+    path("about/", views.about, name="about"),
 ]
-
