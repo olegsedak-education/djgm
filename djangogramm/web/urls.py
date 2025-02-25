2a96258c6a76992b6689b59c1513c2c25067251b
olegsedak-education <olegsedak-education@users.noreply.github.com>
2025-02-25 22:14:15 +0200

    djangogramm urls.py app sync with dev in ide

--- djangogramm/web/urls.py
+++ djangogramm/web/urls.py
@@ -5,46 +5,37 @@ from django.conf import settings
 from . import views
 
 
-defined_user_urls = [
-    path("", views.user_profile, name="user_profile"),
-    path("follow/", views.follow_user, name="follow_user"),
-    path("unfollow/", views.unfollow_user, name="unfollow_user")
+app_name = 'web'
+
+auth_patterns = [
+    path("register/", views.sign_up, name="register"),
+    path("register/complete/<uuid:token>/", views.complete_registration, name="registration_complete"),
+    path("login/", views.sign_in, name="login"),
+    path("logout/", LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name="logout"),
 ]
 
-defined_post_urls =[
-    path("", views.post_detail, name="post_detail"),
-    path("like/", views.like_post, name="like_post"),
-    path("unlike/", views.unlike_post, name="unlike_post"),
-    path("dislike/", views.dislike_post, name="dislike_post"),
-    path("undislike/", views.undislike_post, name="undislike_post")
+user_patterns = [
+    path("", views.users_list, name="user_list"),
+    path("<int:pk>/", views.user_profile, name="user_detail"),
+    path("<int:pk>/follow/", views.follow_user, name="follow"),
+    path("<int:pk>/unfollow/", views.unfollow_user, name="unfollow"),
 ]
 
+post_patterns = [
+    path("", views.posts_list, name="post_list"),
+    path("create/", views.create_post, name="create"),
+    path("<int:pk>/", views.post_detail, name="detail"),
+    path("<int:pk>/like/", views.like_post, name="like"),
+    path("<int:pk>/unlike/", views.unlike_post, name="unlike"),
+    path("<int:pk>/dislike/", views.dislike_post, name="dislike"),
+    path("<int:pk>/undislike/", views.undislike_post, name="undislike"),
+]
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
-    path("accounts/",
-         include([
-             path("register/", views.sign_up, name="register"),
-             path("login/", views.sign_in, name="login"),
-             path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
-             ])
-         ),
-    path("users/",
-         include([
-             path("", views.users_list, name="users_list"),
-             path("<int:user_id>/",
-                  include(defined_user_urls)),
-             ])
-         ),
-    path("posts/",
-         include([
-             path("", views.posts_list, name="posts_list"),
-             path("create/", views.create_post, name="create_post"),
-             path("<int:post_id>/",
-                  include(defined_post_urls),
-                  ),
-         ]),
-    ),
-    path("feed/", views.feed, name="feed"),
     path("about/", views.about, name="about"),
+    path("accounts/", include((auth_patterns, "auth"))),
+    path("users/", include((user_patterns, "users"))),
+    path("posts/", include((post_patterns, "posts"))),
+    path("feed/", views.feed, name="feed"),
 ]
